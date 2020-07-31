ac6dd7fd70be9f07fb8c5377ce3bab45ee5d1cbd
MacBook <alinauroze@hotmail.com>
2020-07-31 10:55:03 +0500

    save changes

--- components/activities/Recipies.tsx
+++ components/activities/Recipies.tsx
@@ -1,13 +1,30 @@
 import React from 'react'
-import {Text, View, Dimensions, StyleSheet} from 'react-native'
+import {Text, ScrollView, View, Dimensions, StyleSheet} from 'react-native'
 
-import RecipeCard from '../cards/Recipe'
+import RecipeCard from '../cards/recipe'
 
 let data = [
     {
         name: "Tea",
         image: "https://s23991.pcdn.co/wp-content/uploads/fly-images/102645/spiced-sweet-milk-tea-recipe-fp-400x400-c.jpg",
         recipe: "Step1\nStep2\nStep3"
+    },
+    {
+        name: "Lemon Pepper Chicken",
+        image: "https://hips.hearstapps.com/delish/assets/17/35/1504195270-lemon-pepper-chicken.jpg",
+        recipe: "Step1\nStep2\nStep3"
+    },
+    {
+        name: "Roasted Chicked Cassarole",
+        image: "https://images-gmi-pmc.edge-generalmills.com/6deec5df-0a0b-4619-8472-a60049fcde72.jpg"
+    },
+    {
+        name: "Butter Chicken",
+        image: "https://www.recipetineats.com/wp-content/uploads/2019/01/Butter-Chicken_5-SQ.jpg"
+    },
+    {
+        name: "Seasme Ginger Chicken",
+        image: "https://www.sprinklesandsprouts.com/wp-content/uploads/2019/10/Takeout-Style-Sesame-Ginger-Chicken-SQ.jpg"
     }
 ]
 
@@ -19,9 +36,30 @@ export default class Recipies extends React.Component {
 
     render () {
         return (
+            <ScrollView>
             <View style={styles.container}>
-
+                {
+                    data.map(recipe => {
+                        return (
+                            <RecipeCard 
+                                cover={recipe.image}
+                                name={recipe.name}
+                            />
+                        )
+                    })
+                }
+                {
+                    data.map(recipe => {
+                        return (
+                            <RecipeCard 
+                                cover={recipe.image}
+                                name={recipe.name}
+                            />
+                        )
+                    })
+                }
             </View>
+            </ScrollView>
         )
     }
 }
@@ -30,6 +68,12 @@ const styles = StyleSheet.create({
     container : {
         marginTop: 50,
         width: Dimensions.get('window').width - 30,
-        padding: 15
+        padding: 15,
+        flexDirection:'row',
+        flexWrap:'wrap',
+        textAlign: "center"
     },
+    card: {
+        margin: 10
+    }
 })
--- components/cards/recipe.js
+++ components/cards/recipe.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {View, ImageBackground, Text, StyleSheet, Dimension} from 'react-native'
+import {View, ImageBackground, Text, StyleSheet, Dimensions, Alert} from 'react-native'
 
 export default class RecipeCard extends React.Component {
     constructor(props) {
@@ -7,12 +7,47 @@ export default class RecipeCard extends React.Component {
     }
     render () {
         return (
-            <View style = {Object.assign(styles.container, )}>
-                <ImageBackground source = {this.props.cover}>
-
+            <View style = {styles.container}>
+                <ImageBackground source = {{uri: this.props.cover}} style={styles.bg}>
+                    <View style={styles.fg}>
+                        <Text style={styles.fg_text}>{this.props.name}</Text>
+                    </View>
                 </ImageBackground>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        width: Dimensions.get('window').width/2 - 45,
+        height: Dimensions.get('window').width/2 - 45,
+        borderRadius: 10,
+        overflow: "hidden",
+        margin: 7,
+        alignItems: "flex-start",
+        flexWrap: 'wrap'
+    },
+    bg: {
+        width: "100%",
+        height: "100%",
+        borderRadius: 50
+    },
+    fg: {
+        width: "100%",
+        height: "100%",
+        backgroundColor: "#00000088",
+        textAlignVertical: "center",
+        textAlign: "center",
+        color: "white",
+    },
+    fg_text: {
+        textAlignVertical: "center",
+        color: "white",
+        fontSize: 22,
+        textAlign: "center",
+        fontWeight: "bold",
+        marginTop: Dimensions.get('window').width/4 - 50
+    }
+})
 
